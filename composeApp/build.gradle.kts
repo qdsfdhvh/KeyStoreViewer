fc32978d2861b095a67ee7af49e84ac7fa3a22f8
seiko <seiko_des@outlook.com>
2024-06-25 16:43:58 +0800

    version name to 1.0.0

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -66,7 +66,7 @@ android {
     minSdk = libs.versions.android.minSdk.get().toInt()
     targetSdk = libs.versions.android.targetSdk.get().toInt()
     versionCode = 1
-    versionName = "1.0"
+    versionName = "1.0.0"
   }
   packaging {
     resources {
