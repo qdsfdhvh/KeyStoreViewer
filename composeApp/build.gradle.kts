e329c0d07493f9be6b031dbdc9bdb779409249f4
seiko <seiko_des@outlook.com>
2024-07-02 09:09:00 +0800

    version code to 3

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -65,7 +65,7 @@ android {
     applicationId = "com.seiko.keystoreviewer"
     minSdk = libs.versions.android.minSdk.get().toInt()
     targetSdk = libs.versions.android.targetSdk.get().toInt()
-    versionCode = 2
+    versionCode = 3
     versionName = "1.0.0"
   }
   packaging {
